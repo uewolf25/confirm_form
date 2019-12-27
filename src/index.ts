67dc3e8ac29189556618bd3450acd17ab0e892e5
wolf <nyan643.ym25mm10@gmail.com>
2019-12-28 04:56:23 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// import { SheetService } from './sheet.service';
+// main
+declare var global: any;
+
+global.caconfirm = (): void => {
+  // const ss = SheetService.createInitialFile('New file');
+  // ss.getRange('A2').setValue('Happy gas!');
+};
