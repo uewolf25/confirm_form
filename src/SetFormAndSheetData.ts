5fbf4a9092f302754f292e14e03ad2e46fbef508
wolf <nyan643.ym25mm10@gmail.com>
2019-12-29 04:03:46 +0900

    preparation for using Form and Sheet class

--- src/SetFormAndSheetData.ts
+++ src/SetFormAndSheetData.ts
@@ -1,10 +1,34 @@
-import { Property } from './property';
+import { Property } from './Property';
+import { Form } from './Form';
+import { Sheet } from './Sheet';
 
 export class SetFormAndSheetData{
 
-  constructor(prop: Property){
+  private _prop: Property;
+  private _form: Form;
+  private _sheet: Sheet;
 
+  constructor(prop: Property){
+    this._prop = prop;
+    this.initialize(this._prop);
   }
 
+  private initialize(prop: Property) {
+    this._form = new Form(prop.getForm);
+    this._sheet = new Sheet(prop.getSheetForEveryone, prop.getSheetMenbers);
+  }
+  /**
+   * name
+   */
+  public name(): void {
+    
+  }
+
+  /**
+   * insertSheet
+   */
+  public insertSheet(): void {
+    
+  }
 
 }
