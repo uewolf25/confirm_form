b982842aa8402347c84f35d009e727044e6166f0
wolf <nyan643.ym25mm10@gmail.com>
2019-12-29 04:02:34 +0900

    main method

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Property } from './property';
+import { Property } from './Property';
 import { SetFormAndSheetData } from './SetFormAndSheetData';
 
 // main
@@ -7,4 +7,13 @@ declare var global: any;
 global.caconfirm = (): void => {
   const prop: Property = new Property();
   const setData: SetFormAndSheetData = new SetFormAndSheetData(prop);
+  // try{
+  //   ScriptApp.newTrigger('copyAndPaste')/* 一番初めに実行する関数名を入れる */
+  //   .forForm(FormUrl/* をopenByUrl */)
+  //   .onFormSubmit()
+  //   .create();
+  //   Logger.log('successly create trigger .');
+  // }catch(error){
+  //   Logger.log('Miss: cannot set trigger');
+  // }
 };
