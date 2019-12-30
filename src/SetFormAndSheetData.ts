4db905791a303d437eaf76264f9129c5a50ff653
wolf <nyan643.ym25mm10@gmail.com>
2019-12-30 19:29:06 +0900

    add Debug method

--- src/SetFormAndSheetData.ts
+++ src/SetFormAndSheetData.ts
@@ -2,33 +2,28 @@ import { Property } from './Property';
 import { Form } from './Form';
 import { Sheet } from './Sheet';
 
-export class SetFormAndSheetData{
-
+export class SetFormAndSheetData {
   private _prop: Property;
-  private _form: Form;
+  // private _form: Form;
   private _sheet: Sheet;
 
-  constructor(prop: Property){
+  public constructor(prop: Property) {
     this._prop = prop;
     this.initialize(this._prop);
   }
 
   private initialize(prop: Property) {
-    this._form = new Form(prop.getForm);
+    // this._form = new Form(prop.getForm);
     this._sheet = new Sheet(prop.getSheetForEveryone, prop.getSheetMenbers);
   }
-  /**
-   * name
-   */
-  public name(): void {
-    
-  }
 
   /**
-   * insertSheet
+   * debugMethod
+   * @param
+   * @returns
    */
-  public insertSheet(): void {
-    
+  public debugMethod() {
+    Logger.log(`${typeof this._sheet}型です。`);
+    // this._sheet.getForEveryoneSheetName('sample');
   }
-
 }
--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,15 @@ declare var global: any;
 global.caconfirm = (): void => {
   const prop: Property = new Property();
   const setData: SetFormAndSheetData = new SetFormAndSheetData(prop);
+  setData.debugMethod();
   const formData: Form = new Form(prop.getForm);
-  try {
-    ScriptApp.newTrigger('initialize')
-      .forForm(formData.openApps(FormApp))
-      .onFormSubmit()
-      .create();
-    Logger.log('successly create trigger .');
-  } catch (error) {
-    Logger.log('Miss: cannot set trigger');
-  }
+  // try {
+  //   ScriptApp.newTrigger('initialize')
+  //     .forForm(formData.openApps())
+  //     .onFormSubmit()
+  //     .create();
+  //   Logger.log('successly create trigger .');
+  // } catch (error) {
+  //   Logger.log('Miss: cannot set trigger');
+  // }
 };
